17c9b6b5fb15c0a40e2843cc775f7f5aa8879d7b
pain <legendofpain5112@gmail.com>
2023-03-22 22:46:16 +0500

    add 1 page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-const privacyIcon = document.querySelector(".privacy-icon")
+const privacyIcons = document.querySelectorAll(".privacy-icon")
 const backIcon = document.querySelector(".privacy__back")
-
 const privacy = document.querySelector(".privacy")
+const cards = document.querySelectorAll(".card")
 
-privacyIcon.addEventListener("click", () => {
-	privacy.classList.add("active")
+const order = document.querySelector(".order")
+const orderBack = document.querySelector(".order__back")
+
+privacyIcons.forEach(icon => {
+	icon.addEventListener("click", () => {
+		privacy.classList.add("active")
+	})
 })
 
 backIcon.addEventListener("click", () => {
 	privacy.classList.remove("active")
+})
+
+cards.forEach(card => {
+	card.addEventListener("click", () => {
+		order.classList.add("active")
+	})
+})
+
+orderBack.addEventListener("click", () => {
+	order.classList.remove("active")
 })
